ba9f20a3d44dae818993b80451593c1633f7b87c
prasannakarki77 <prasanna.karki77@gmail.com>
2024-10-23 22:05:02 +0545

    tailswind and shadcn ui setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,41 @@
-'use client'
-import { useEffect, useState } from 'react'
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [message, setMessage] = useState()
+  const [message, setMessage] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch('/api/hello')
-      const { message } = await res.json()
-      setMessage(message)
-    }
-    fetchData()
-  }, [])
+      const res = await fetch("/api/hello");
+      const { message } = await res.json();
+      setMessage(message);
+    };
+    fetchData();
+  }, []);
 
-  if (!message) return <p>Loading...</p>
+  if (!message) return <p>Loading...</p>;
 
-  return <p>{message}</p>
+  return (
+    <div className="text-center ">
+      {message}
+
+      <div className="flex mx-auto max-w-sm items-center space-x-2 ">
+        <Input type="email" placeholder="Email" />
+        <Button type="submit">Subscribe</Button>
+      </div>
+    </div>
+  );
 }
